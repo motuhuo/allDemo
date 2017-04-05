86978b212f98ceb92bfe6b6fd88d807786f651c4
motuhuo <210171692@qq.com>
2017-04-05 09:32:48 +0800

    twoPush

---
+++ vueApp/src/components/child2/child2.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="ratings">
+  	我是ratings child2
+  	<input type="text" v-model="msg" @change="onInput">
+  </div>
+</template>
+
+<script type="ecmascript-6">
+    export default {
+	  data () {
+	    return {
+	    	msg: '请输入值'
+	    }
+	  },
+    methods: {
+      onInput: function () {
+        if (this.msg.trim()) {
+          this.$emit('messageOne', this.msg);
+        }
+      }
+    }
+	}
+</script>
+
+<style lang="stylus" rel="stylesheet"></style>
---
+++ vueApp/src/components/ratings/ratings.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="ratings">
+  	我是ratings
+  	<child2 @messageOne="recieveMessage"></child2>
+    {{ msg | placeholder('Text if msg is missing') }} 
+  </div>
+</template>
+
+<script type="ecmascript-6">
+import child2 from '@/components/child2/child2'
+
+  export default {
+    data () {
+      return {
+        msg: 'ABC'
+      }
+    },
+	  components: {
+      child2
+    },
+    methods: {
+      recieveMessage: function (text) {
+        alert('监听到子组件变化'+text);
+      }
+    }
+	}
+</script>
+
+<style lang="stylus" rel="stylesheet"></style>
